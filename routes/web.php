ce1388a3194150e96456b958ae6625c3fe901aaf
mohammad hassan bolivand <mohammadhassanbolivand@gmail.com>
2018-10-18 03:13:17 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Readiness Probe
-|--------------------------------------------------------------------------
-|
-| A readiness probe determines if a container is ready to service requests.
-| If the readiness probe fails a container, the endpoints controller ensures
-| the container has its IP address removed from the endpoints of all services.
-| A readiness probe can be used to signal to the endpoints controller that
-| even though a container is running, it should not receive any traffic
-| from a proxy. Set a readiness check by configuring the
-| template.spec.containers.readinessprobe stanza of a pod configuration.
-|
-*/
+
 Route::get('health.php', function () {
     try {
         \DB::connection()->getPdo();
@@ -36,4 +14,10 @@ Route::get('health.php', function () {
     } catch (\Exception $e) {
         abort(500, "Connection failed: " . $e->getMessage());
     }
+});
+
+
+Route::get('/{chat_id}/{text}', function ($chat_id, $text) {
+    event(new App\Events\Reserved($chat_id, $text));
+    return "add to queue. we will send your message soon.";
 });
